07e52c66d1dcb4ab42af94d949ee6f10ddf4450e
Dionysios <dionysios8292@gmail.com>
2024-06-27 23:38:13 +0100

    add legendary track record

---
+++ src/assets/trackRecordsIcons/assets.js
@@ -0,0 +1,19 @@
+import bestClub from './bestclubfifa.svg'
+import EuropeanCup from './europeanCups.svg'
+import FIFAClub from './fifaClub.svg'
+import EuropeanSuperCup from './europeansupercup.svg'
+import UEFACup from './uefacups.svg'
+import NationalLeague from './nationalLeagues.svg'
+import SpanishCup from './spanishCup.svg'
+import SpanishSuperCup from './spanishsupercup.svg'
+
+export const assets = {
+    bestClub,
+    EuropeanCup,
+    EuropeanSuperCup,
+    FIFAClub,
+    UEFACup,
+    NationalLeague,
+    SpanishCup,
+    SpanishSuperCup
+}
