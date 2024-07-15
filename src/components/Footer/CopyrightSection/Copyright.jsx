86e97157ec7ad9aa87ff1279605b988c38df3eec
Dionysios <dionysios8292@gmail.com>
2024-07-15 14:31:03 +0100

    add github personal page

--- src/components/Footer/CopyrightSection/Copyright.jsx
+++ src/components/Footer/CopyrightSection/Copyright.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import "./Copyright.css";
 import { LuDot } from "react-icons/lu";
 import { assets } from "../../../assets/assets";
+import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Copyright = () => {
   return (
@@ -41,7 +43,12 @@ const Copyright = () => {
           <LuDot style={{ color: "#0f2145", fontSize: "12px" }} />
           <a>realmadrid.com</a>
         </div>
-        <div className="text">Copyright this is the </div>
+        <div className="text">
+          <Link to='https://github.com/DionisisChytiris?tab=repositories' style={{display: 'flex',flexDirection: 'row', gap: 10, alignItems: 'center', backgroundColor: '#ccc',padding: '4px 8px', borderRadius: 20}}>
+            <div>DionisisChytiris GitHub</div>
+            <div><FaGithub style={{ color: "#0f2145", fontSize: "16px" }}/></div>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/pages/NavPages/Madridistas.jsx
+++ src/pages/NavPages/Madridistas.jsx
@@ -1,8 +1,42 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import './styles/Madridistas.css'
+import axios from "axios";
+
+const baseURL = "https://real-madrid-team-fast-api.vercel.app/"
 
 const Madridistas = () => {
+  const [post, setPost] = useState(
+    [
+      {
+        position: '',
+        firstname: ''
+      }
+    ]
+  )
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://real-madrid-team-fast-api.vercel.app/');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        setPost(response.data);
+        console.log(response.data)
+        // setLoading(false);
+      } catch (err) {
+        console.log(err)
+        // setError(err.message);
+        // setLoading(false);
+      }
+    }
+      fetchData()
+  },[])
+
+  // if (!post) return null;
+
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -13,14 +47,27 @@ const Madridistas = () => {
     setIsClicked(!isClicked);
   };
 
+
   return (
+    <>
+  
     <div style={container}>
       <div 
        className={`box ${isClicked ? 'clicked' : ''}`}
         onClick={handleClick}
         // style={{padding: '40px', backgroundColor: 'lightgreen',transition:' backgroundColor 0.3s ease, transform 0.3s ease'}}
-      >Madridistas</div>
+      >
+       {post.map((item) => (
+          <div key={item.id}> 
+            <div>Position: {item.position}</div>
+            <div>Name: {item.firstname}</div>
+            <div>Name: {item.firstname}</div>
+          </div>
+        ))}
+      </div>
+    
     </div>
+      </>
   );
 };
 
