7ff6cea156548a20a44172e2e1f459ddfb6d6755
Dionysios <dionysios8292@gmail.com>
2025-07-20 10:21:37 +0100

    sdf

--- src/pages/NavPages/Madridistas.jsx
+++ src/pages/NavPages/Madridistas.jsx
@@ -1,5 +1,6 @@
-import  { useEffect, useState } from "react";
-import './styles/Madridistas.css';
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
+import "./styles/Madridistas.css";
 import axios from "axios";
 
 const baseURL = "https://real-madrid-team-fast-api.vercel.app/";
@@ -7,20 +8,21 @@ const baseURL = "https://real-madrid-team-fast-api.vercel.app/";
 const Madridistas = () => {
   const [players, setPlayers] = useState([]);
   const [isClicked, setIsClicked] = useState(false);
+const [error, setError] = useState("");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(baseURL);
-        setPlayers(response.data);
-        console.log(response.data);
-      } catch (err) {
-        console.error("Error fetching players:", err);
-      }
-    };
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(baseURL);
+      setPlayers(response.data);
+    } catch (err) {
+      console.error("Error fetching players:", err);
+      setError("Failed to load player data. Please try again later.");
+    }
+  };
 
-    fetchData();
-  }, []);
+  fetchData();
+}, []);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -31,21 +33,41 @@ const Madridistas = () => {
   };
 
   return (
-    <div className="madridistas-container">
-      <div
-        className={`box ${isClicked ? 'clicked' : ''}`}
-        onClick={handleClick}
-      >
-        {players.map((item) => (
-          <div key={item.id}>
-            <div>Position: {item.position}</div>
-            <div>Name: {item.firstname} {item.surname}</div>
-            <hr />
-          </div>
-        ))}
+    <>
+      <div style={container}>
+        <div
+          className={`box ${isClicked ? "clicked" : ""}`}
+          onClick={handleClick}
+          // style={{padding: '40px', backgroundColor: 'lightgreen',transition:' backgroundColor 0.3s ease, transform 0.3s ease'}}
+        >
+          {players.length === 0 ? (
+            <p>Loading players...</p>
+          ) : (
+            players.map((item) => (
+              <div key={item.id}>
+                <div>Position: {item.position}</div>
+                <div>
+                  Name: {item.firstname} {item.surname}
+                </div>
+                <hr />
+              </div>
+            ))
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
 export default Madridistas;
+
+const container = {
+  width: "80%",
+  height: "100vh",
+  backgroundColor: "lightgray",
+  margin: "auto",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  marginBottom: "30px",
+};
